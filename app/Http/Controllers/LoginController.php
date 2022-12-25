e27952e8f1f569b012d719f3eefdb6ea43cb0088
Ahlul Aziz Ancala Putra <ahlulazizap@gmail.com>
2022-12-25 18:18:07 +0700

    minor changes on login

--- app/Http/Controllers/LoginController.php
+++ app/Http/Controllers/LoginController.php
@@ -21,7 +21,7 @@ class LoginController extends Controller
         if (Auth::attempt($credentials)) {
             $request->session()->regenerate();
  
-            return redirect()->intended('admin');
+            return redirect()->intended();
         }
 
         return back()->with('loginError','Your email or password does not match our records');
