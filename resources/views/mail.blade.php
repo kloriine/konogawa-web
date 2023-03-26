de6aac2b9029d2614a3698ce5177486eaa529c73
Ahlul Aziz Ancala Putra <ahlulazizap@gmail.com>
2023-03-26 20:35:44 +0700

    update mail interface

--- resources/views/mail.blade.php
+++ resources/views/mail.blade.php
@@ -1,7 +1,6 @@
-<h2>Hello</h2> <br><br>
-You have got an email from : {{ $name }} <br><br>
-User details: <br><br>
-Name: {{ $name }} <br>
+<h2>Hello Admin</h2><br>
+You've got a new email from Mr./Mrs. {{ $name }} <br><br>
+Below are the details from the email,<br><br>
 Email: {{ $email }} <br>
 Subject: {{ $subject }} <br>
 Message: {{ $user_query }} <br><br>
