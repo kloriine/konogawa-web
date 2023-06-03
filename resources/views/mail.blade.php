32776c1355ee9d9f36e91795ecb41e281b9f97ea
Ahlul Aziz <ahlulazizap@gmail.com>
2023-06-03 15:57:36 +0700

    delete email view

--- resources/views/mail.blade.php
+++
@@ -1,7 +0,0 @@
-<h2>Hello Admin</h2><br>
-You've got a new email from Mr./Mrs. {{ $name }} <br><br>
-Below are the details from the email,<br><br>
-Email: {{ $email }} <br>
-Subject: {{ $subject }} <br>
-Message: {{ $user_query }} <br><br>
-Thanks
