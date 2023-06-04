7837463016bc89064e00aec5eff904702675fa5c
Ahlul Aziz <ahlulazizap@gmail.com>
2023-06-04 16:27:35 +0700

    max 9 news at one page

--- app/Http/Controllers/NewsController.php
+++ app/Http/Controllers/NewsController.php
@@ -8,7 +8,7 @@ use App\Models\News;
 class NewsController extends Controller
 {
     public function index () {
-        $news = News::orderBy('id', 'desc')->get();
+        $news = News::orderBy('id', 'desc')->paginate(9);
         return view('news', ['news' => $news]);
     }
 }
